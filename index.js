930132a86ae825e818889221e7adf7711d8b0c47
Abdiqani <abdiqaniadan254@gmail.com>
2023-10-13 16:44:38 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names) {
+    
+    let thankYouMessages = [];
+  
+    
+    for (let i = 0; i < names.length; i++) {
+      
+      let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    
+    return thankYouMessages;
+  }
+ 
+function countDown() {
+    let countDown=10;
+    while (countDown>-1){
+        console.log (countDown--)
+    }
+    return countDown;
+}
